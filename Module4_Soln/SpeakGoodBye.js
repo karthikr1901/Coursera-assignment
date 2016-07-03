ac87e01dce6ad83bce82de6506a62753939ba852
karthikr1901 <karthikr1901@gmail.com>
2016-07-03 23:31:04 +0800

    Mod 4 Soln step 6

--- Module4_Soln/SpeakGoodBye.js
+++ Module4_Soln/SpeakGoodBye.js
@@ -3,7 +3,7 @@
 
 // STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
 // See Lecture 52, part 2
-
+(function(window){
 
 // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
@@ -20,3 +20,4 @@ var speakWord = "Good Bye";
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
 // xxxx.xxxx = byeSpeaker;
+})(window);
